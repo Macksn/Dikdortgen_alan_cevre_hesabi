3f46a5e9ff0cae4f9b91aaa8b4d15aba076571f8
Macksn <itsmacksn@yahoo.com>
2024-03-24 22:29:12 +0300

    Dikdortgen_alan_cevre_hesabi
    Dikdörtgen alan ve çevre hesabı

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DİKDÖRTGENİN_ALAINI_CEVRESI_İF_ELSE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int uzunkenar, kısakenar, alan, çevre, sonuç;
+            uzunkenar = Convert.ToInt16(textBox1.Text);
+            kısakenar = Convert.ToInt16(textBox2.Text);
+
+            if (radioButton1.Checked)
+            {
+               
+                alan = kısakenar * uzunkenar;
+                label1.Text = alan.ToString();
+            }
+            else if (radioButton2.Checked)
+            {
+               
+                çevre = (kısakenar + uzunkenar) * 2;
+                label1.Text = çevre.ToString();
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DİKDÖRTGENİN_ALAINI_CEVRESI_İF_ELSE
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
